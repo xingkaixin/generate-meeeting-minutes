693312c40cf75c6e19b551d67e9c706c95474973
XingKaiXin <xingkaixin@gmail.com>
2023-04-10 23:05:31 +0800

    refactor: Refactor OpenAI integration in main.py
    - Move OpenAI API configuration to env variable
    - Refactor `read_srt` function to accept file path parameter
    - Extract `split_ask_answer` function from main loop
    - Refactor main function to call `split_ask_answer` and `ask_ai` separately
    - Update question prompt in `split_ask_answer` function

--- main.py
+++ main.py
@@ -1,54 +1,73 @@
 from pathlib import Path
-from config import openai_config
+
 import openai
 
-srt_file = Path("./data/20230410.wav.srt")
-
-with srt_file.open(mode="r", encoding="utf-8") as f:
-    srt = f.read()
+from config import openai_config
 
 
-lines = srt.strip().split("\n")
-subs = []
-sub = {}
+def read_srt(srt_file):
+    with srt_file.open(mode="r", encoding="utf-8") as f:
+        srt = f.read()
 
-for i, line in enumerate(lines):
-    if i % 4 == 0:
-        if sub.get("id", None) is not None:
-            subs.append(sub)
-        sub = {"id": int(line)}
-    elif i % 4 == 1:
-        start, end = line.split(" --> ")
-        sub["start"] = start
-        sub["end"] = end
-    elif i % 4 == 2:
-        sub["text"] = line
+    lines = srt.strip().split("\n")
+    subs = []
+    sub = {}
 
-if sub is not None:
-    subs.append(sub)
+    for i, line in enumerate(lines):
+        if i % 4 == 0:
+            if sub.get("id", None) is not None:
+                subs.append(sub)
+            sub = {"id": int(line)}
+        elif i % 4 == 1:
+            start, end = line.split(" --> ")
+            sub["start"] = start
+            sub["end"] = end
+        elif i % 4 == 2:
+            sub["text"] = line
 
-subarrays = [subs[i : i + 100] for i in range(0, len(subs), 100)]  # noqa
+    if sub is not None:
+        subs.append(sub)
+    return subs
 
-answer = []
 
-for subarray in subarrays:
-    sub_question = "\n".join([sub["text"] for sub in subarray])
-    quesiton = "仔细阅读以下内容，我要用于撰写会议纪要.\n" f"{sub_question}\n"
+def split_ask_answer(subs):
+    subarrays = [subs[i : i + 100] for i in range(0, len(subs), 100)]  # noqa
 
+    answer = []
+
+    for subarray in subarrays:
+        sub_question = "\n".join([sub["text"] for sub in subarray])
+        quesiton = "仔细阅读以下内容，我要用于撰写会议纪要.\n" f"{sub_question}\n"
+
+        answer.append(ask_ai(quesiton))
+
+    return answer
+
+
+def ask_ai(question):
     openai.api_key = openai_config.key
     openai.api_base = openai_config.base_url
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": quesiton}],
+        messages=[{"role": "user", "content": question}],
         temperature=0.3,
         max_tokens=2048,
     )
     if "choices" in response:
-        answer.append(
+        return (
             response["choices"][0].get("message").get("content").encode("utf8").decode()
         )
     else:
         raise ValueError("Invalid response from ChatGPT")
 
 
-print(answer)
+def main():
+    srt_file = Path("./data/20230410.wav.srt")
+    subs = read_srt(srt_file)
+    answers = "\n".join(split_ask_answer(subs))
+    question = f"仔细阅读下面的内容，生成一份有着10年经验的项目经理会议纪要。\n {answers}"
+    print(ask_ai(question))
+
+
+if __name__ == "__main__":
+    main()
